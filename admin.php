9ad79fca743e8787bcc3f88a5ab2ec2279133a05
avraam500 <dablyat16@gmail.com>
2024-11-13 10:44:06 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,1 +1,18 @@
 
+<? include("includes/db.php");
+$echo = "Тут скоро что-то будет";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>AДминкa</title>
+<link rel="stylesheet" href="admin.css">
+</head>
+<body>
+<div class="wrapper">
+<main class="main" id="main">
+<?echo $echo; ?>
+</main>
+</div>
+</body>
+</html>
