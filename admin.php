27616726ca317ab8d33570878022d4ea5b450be1
avraam500 <dablyat16@gmail.com>
2024-11-13 10:50:42 +0300

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,18 @@
+
+<? include("includes/db.php");
+$echo = "Тут скоро что-то будет";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>AДминкa</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="wrapper">
+<main class="main" id="main">
+<?echo $echo; ?>
+</main>
+</div>
+</body>
+</html>
